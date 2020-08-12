6639fa76bed3c0560679c1769cb5c0e87309b006
Andrew Ford <andrew.ford@waltonstudent.org>
2020-08-12 16:35:38 +0000

    New structure, added for loop file

---
+++ src/forloop.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "forloop.h"
+
+void main(){
+	int arr[5];
+
+	for(int i = 0; i < 5; i++){
+		arr[i] = i;
+	}
+
+	for(int i = 0; i < 5; i++){
+		printf("Index %d, value %d\n",i,arr[i]);
+	}
+}

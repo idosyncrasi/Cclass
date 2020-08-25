74253c8ebe4ea5a9e896a0d84d009c2913c94edb
idosyncrasi <28535876+idosyncrasi@users.noreply.github.com>
2020-08-25 10:28:25 -0400

    Update mazeGen.c

--- src/mazeGen.c
+++ src/mazeGen.c
@@ -10,20 +10,20 @@ int getNum(int lower, int upper){
     return(num);
 }
 
-struct point{
-	int x, y;
-};
-struct point origin   = { 0,  0},
+typedef struct point{
+    int x, y;
+}point;
+point origin   = {0,  0},
              errGen   = {-1, 0},
              errOOB   = {-1, 1},
              errNoDir = {-1, 2};
 
-struct point pickDir(int maze[mW][mH], struct point pnt){
+point pickDir(int maze[mW][mH], struct point pnt){
 
-    int northPos,
-        eastPos,
-        southPos,
-        westPos;
+    int northPos = 1,
+        eastPos  = 1,
+        southPos = 1,
+        westPos  = 1;
 
     // North
     if(pnt.y-1 < 0 ){
@@ -42,16 +42,16 @@ struct point pickDir(int maze[mW][mH], struct point pnt){
         eastPos = 0;
     }
 
-    struct point n,e,s,w;
+    point n,e,s,w;
 
     if(northPos != 0){
-        n = { pnt.x, pnt.y-1 };
+        point n = { pnt.x, pnt.y-1 };
     }else if(eastPos != 0){
-        e = { pnt.x+1, pnt.y };
+        point e = { pnt.x+1, pnt.y };
     }else if(southPos != 0){
-        s = { pnt.x, pnt.y+1 }
+        point s = { pnt.x, pnt.y+1 };
     }else if(westPos != 0){
-        w = { pnt.x-1, pnt.y };
+        point w = { pnt.x-1, pnt.y };
     }
 
     /*
@@ -61,58 +61,58 @@ struct point pickDir(int maze[mW][mH], struct point pnt){
 				 w = { pnt.x-1, pnt.y };
      */
 
-	if(&maze[n.x][n.y] == 0 && northPos != 0){
-	    northPos = 1;
-	}else{
-		northPos = 0;
-	}
-	if(&maze[e.x][e.y] == 0 && eastPos != 0){
-	    eastPos = 1;
-	}else{
-		eastPos = 0;
-	}
-	if(&maze[s.x][s.y] == 0 && southPos != 0){
-	    southPos = 1;
-	}else{
-		southPos = 0;
-	}
-	if(&maze[w.x][w.y] == 0 && westPos != 0){
-	    westPos = 1;
-	}else{
-		westPos = 0;
-	}
+    if(&maze[n.x][n.y] == 0 && northPos != 0){
+        northPos = 1;
+    }else{
+        northPos = 0;
+    }
+    if(&maze[e.x][e.y] == 0 && eastPos != 0){
+        eastPos = 1;
+    }else{
+        eastPos = 0;
+    }
+    if(&maze[s.x][s.y] == 0 && southPos != 0){
+        southPos = 1;
+    }else{
+        southPos = 0;
+    }
+    if(&maze[w.x][w.y] == 0 && westPos != 0){
+        westPos = 1;
+    }else{
+        westPos = 0;
+    }
 
-	int num =  getNum(0, 3);
+    int num =  getNum(0, 3);
 
-	// TODO: Fix; doesnt pick correct directions based on available directions && test
-	if(num == 0 && northPos == 1){
-	    return n;
-	}else if(num == 1 && eastPos == 1){
-		return e;
-	}else if(num == 2 && southPos == 1){
-		return s;
-	}else if(num == 3 && westPos == 1){
-		return w;
-	}else{
-		return errNoDir;
-	}
+    // TODO: Fix; doesnt pick correct directions based on available directions && test
+    if(num == 0 && northPos == 1){
+        return n;
+    }else if(num == 1 && eastPos == 1){
+        return e;
+    }else if(num == 2 && southPos == 1){
+        return s;
+    }else if(num == 3 && westPos == 1){
+        return w;
+    }else{
+        return errNoDir;
+    }
 }
 
 void mazeInit(int maze[mW][mH]){
-	for(int i = 0; i < mW; i++){
-		for(int v = 0; v < mH; v++){
-			maze[i][v] = 0;
-		}
-	}
+    for(int i = 0; i < mW; i++){
+        for(int v = 0; v < mH; v++){
+            maze[i][v] = 0;
+        }
+    }
 }
 
 void printMaze(int maze[mW][mH]){
-	for(int i = 0; i < mW; i++){
-		for(int v = 0; v < mH; v++){
-			printf("%d ",maze[i][v]);
-		}
-		printf("\n");
-	}
+    for(int i = 0; i < mW; i++){
+        for(int v = 0; v < mH; v++){
+            printf("%d ",maze[i][v]);
+        }
+        printf("\n");
+    }
 }
 
 bool equalTo(struct point pnt1, struct point pnt2){
@@ -124,26 +124,26 @@ bool equalTo(struct point pnt1, struct point pnt2){
 }
 
 int main(){
-	int maze[mW][mH];
+    int maze[mW][mH];
 
-	mazeInit(maze);
+    mazeInit(maze);
 
-	printMaze(maze);
+    printMaze(maze);
 
-	struct point res = pickDir(maze, origin);
+    point res = pickDir(maze, origin);
 
-	if(equalTo(res, errGen)){
-	    printf("Error");
-	    return 1;
-	}else if(equalTo(res, errOOB)){
-	    printf("Index out of bounds");
-	    return 2;
-	}else if(equalTo(res, errNoDir)){
-	    printf("No direction found");
-	    return 3;
-	}else{
+    if(equalTo(res, errGen)){
+        printf("Error");
+        return 1;
+    }else if(equalTo(res, errOOB)){
+        printf("Index out of bounds");
+        return 2;
+    }else if(equalTo(res, errNoDir)){
+        printf("No direction found");
+        return 3;
+    }else{
 
-	}
+    }
 
-	return 0;
+    return 0;
 }
